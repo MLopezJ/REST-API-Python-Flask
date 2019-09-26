480b802f360a6adb3a947622e6eb2780bcfa1792
Mauro Lopez <mauro.lopez@avantica.net>
2019-09-26 17:02:52 -0600

    added auth blueprint in init file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from . import db
+from . import auth
 
 def create_app(test_config=None):
     # create and configure the app
@@ -29,4 +30,5 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     db.init_app(app)
+    app.register_blueprint(auth.bp)
     return app
